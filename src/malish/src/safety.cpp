a6bba3f82372e7d598c7b86a0da6ea4607e44773
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2018-02-27 20:24:46 +0300

    The first try to send alarm message.
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- src/malish/src/safety.cpp
+++ src/malish/src/safety.cpp
@@ -13,6 +13,9 @@
 #include "std_msgs/String.h"
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/Odometry.h>
+//#include <tf/Quaternion.h>
+#include <tf/transform_datatypes.h>
+#include <tf/LinearMath/Transform.h>
 #include <malish/Obstacle.h>
 #include <geometry_msgs/Pose.h>
 #include "opencv2/highgui/highgui.hpp"
@@ -36,6 +39,12 @@ static const float robot_width = 0.45;
 static const float robot_length = 0.65;
 // Margin of safety vicinity, meters.
 static const float margin = 1;
+// Minimal blob area to consider it as obstacle, [pixels].
+static const float min_blob_area = 10;
+
+//typedef std::vector<cv::Point2f> cvContour;
+typedef std::vector<cv::Point> cvContour;
+
 
 struct mapInfo {
   float resolution;
@@ -44,6 +53,21 @@ struct mapInfo {
   unsigned int height;
 };
 
+/**
+ * Center of mass by moments.
+ */
+cv::Point2f center_by_moments(cvContour const& cnt) {
+    cv::Moments M = cv::moments(cnt);
+    float Cx = int(M.m10 / M.m00);
+    float Cy = int(M.m01 / M.m00);
+    return cv::Point2f(Cx, Cy);
+}
+
+/**
+ *
+ * @param mat
+ * @param map
+ */
 void map_to_mat(cv::Mat & mat, const nav_msgs::OccupancyGrid & map) {
     for(unsigned int i = 0; i < map.info.height; ++i) {
         for(unsigned int j = 0; j < map.info.width; ++j) {
@@ -134,14 +158,15 @@ class Safety {
       float robo_x = ( - map_info.origin.position.x + odom.pose.pose.position.x) / map_info.resolution;
       float robo_y = ( - map_info.origin.position.y + odom.pose.pose.position.y) / map_info.resolution;
 
-      ROS_INFO("odom.pose.pose.position.x: %f, odom.pose.pose.position.y: %f", odom.pose.pose.position.x, odom.pose.pose.position.y);
+      /* ROS_INFO("odom.pose.pose.position.x: %f, odom.pose.pose.position.y: %f", odom.pose.pose.position.x, odom.pose.pose.position.y);
       ROS_INFO("map_info.origin.position.x: %f, map_info.origin.position.y: %f", map_info.origin.position.x, map_info.origin.position.y);
       ROS_INFO("robo_x: %f, robo_y: %f", robo_x, robo_y);
-      ROS_INFO("map_info.resolution %f", map_info.resolution);
+      ROS_INFO("map_info.resolution %f", map_info.resolution);*/
 
       // Render robot and vicinity into map's sized image.
       // Binary image with robot
       cv::Mat tmp_img = cv::Mat::zeros(map_image.size(), CV_8UC1);
+      cv::Mat intersect_img = cv::Mat::zeros(map_image.size(), CV_8UC1);
 
       // tmp_img
       int radius = static_cast<int>((robot_length / 2.0 + margin) / map_info.resolution);
@@ -155,23 +180,68 @@ class Safety {
       if(cv::waitKey(1000.0 / MFPS) == 27)
           return;
 
-      // Find intersection.
-      // cv::Mat intersect_map = tmp_img * map_image;
-      cv::bitwise_and(tmp_img, map_image, tmp_img);
+      // Find intersection map.
+      cv::bitwise_and(tmp_img, map_image, intersect_img);
 
       /// Show in a window.
       cv::namedWindow( "Intersection", CV_WINDOW_NORMAL);
-      cv::imshow("Intersection", tmp_img * 255);
+      cv::imshow("Intersection", intersect_img * 255);
       if(cv::waitKey(1000.0 / MFPS) == 27)
           return;
 
-      // Analyse obstacles.
+      /// Find contours on intersection map.
+      std::vector<cvContour> contours;
+      std::vector<cv::Vec4i> hierarchy;
+      cv::findContours(tmp_img, contours, hierarchy, cv::RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0));
 
-      // Fill out safety message form.
-      // safety_msg_
+      // Analyse obstacles. Find blob with maximal area.
+      float max_blob_area = 0;
+      cvContour best_contour;
+      for(int i = 0; i < contours.size(); i++) {
+          if(cv::contourArea(contours[i]) > max_blob_area) {
+              max_blob_area = cv::contourArea(contours[i]);
+              best_contour = contours[i];
+          }
+      }
 
-      // Publish safety alarm message.
-      safety_pub_.publish(safety_msg_);
+      //
+      if(max_blob_area > min_blob_area) {
+          // Find center of blob.
+          cv::Point2f center_blob = center_by_moments(best_contour);
+
+          ROS_INFO("center_blob_x: %f, center_blob_y: %f", center_blob.x, center_blob.y);
+          ROS_INFO("max_blob_area: %f", max_blob_area);
+
+          // Fill out safety message form.
+          safety_msg_.alert = true;
+
+          /*tf::Matrix3x3 obs_mat;
+          obs_mat.setEulerYPR(Yaw,Pitch,Roll);
+
+          tf::Quaternion q_tf;
+          obs_mat.getRotation(q_tf);
+          INS_msg.orientation.x = q_tf.getX();
+          INS_msg.orientation.y = q_tf.getY();
+          INS_msg.orientation.z = q_tf.getZ();
+          INS_msg.orientation.w = q_tf.getW();*/
+
+          float angle = atan2(center_blob.x, center_blob.y);
+          tf::Quaternion q_tf;
+          q_tf.setEuler(angle, 0, 0);
+
+          safety_msg_.pos.orientation.w = 0;
+          safety_msg_.pos.orientation.x = 0;
+          safety_msg_.pos.orientation.y = 0;
+          safety_msg_.pos.orientation.z = 0;
+
+          safety_msg_.pos.position.x = center_blob.x;
+          safety_msg_.pos.position.y = center_blob.y;
+          safety_msg_.pos.position.z = 0;
+
+          // Publish safety alarm message.
+          safety_pub_.publish(safety_msg_);
+      }
+
   }
 
   protected:;
