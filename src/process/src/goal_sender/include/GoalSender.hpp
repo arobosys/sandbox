5d2a9f2cdeb453e7a1c6c7884064b071a8474438
zhuhua <zhuhua1302@gmail.com>
2018-03-01 12:28:00 +0300

    B1-35 change process goal_sender, it works if the robot in scenario Exhihibition, it does not work if the robot in scenario Manish

--- src/process/src/goal_sender/include/GoalSender.hpp
+++ src/process/src/goal_sender/include/GoalSender.hpp
@@ -3,23 +3,34 @@
 #include <std_msgs/String.h>
 #include <process_interface/process_interface.hpp>
 
+
 class GoalSender {
+    /*
     const std::string GOAL1 = "2D_goal1";
     const std::string GOAL2 = "2D_goal2";
     const std::string GOAL3 = "2D_goal3";
     const std::string GOAL4 = "2D_goal4";
-    //const uint32_t QUEUE_SIZE = 100;
-    //const int TF_TIMEOUT_SEC = 5;
+    const uint32_t QUEUE_SIZE = 100;
+    const int TF_TIMEOUT_SEC = 5;
+     */
+
+    const std::string NUM_GOAL = "num_goal";
+    const std::string GOAL1 = "2d_goal1";
+    const std::string GOAL2 = "2d_goal2";
+    const std::string GOAL3 = "2d_goal3";
+    const std::string GOAL4 = "2d_goal4";
+
+    int num_goal=2;
 
     std::shared_ptr<interface::ProcessInterface> rosLinkClientPtr = nullptr;
 
-    void startCallback(const std_msgs::String::ConstPtr &msg);
+    //void startCallback(const std_msgs::String::ConstPtr &msg);
 
     void goalCallback(const interface::ProcessInterface::Parameters &params);
 
     void preemtCallback();
 
-   // void parseTransforms(const std::map<std::string, std::string> &keyToValue);
+    void parseTransforms(const std::map<std::string, std::string> &keyToValue);
 
 public:
     /**
--- src/process/src/goal_sender/src/goal_sender_class.cpp
+++ src/process/src/goal_sender/src/goal_sender_class.cpp
@@ -7,6 +7,7 @@ GoalSender::GoalSender(ros::NodeHandle &handle, int argc, char **argv) {
     //goalListener = handle.subscribe(INPUT_TOPIC_NAME, 1, &GoalSenser::startCallback, this);
 //rosLinkClientPtr = std::make_shared<interface::ProcessInterface>(argc, argv);
 
+
     rosLinkClientPtr = std::make_shared<interface::ProcessInterface>(argc, argv,
                                                                      std::bind(&GoalSender::goalCallback, this,
                                                                                std::placeholders::_1),
@@ -14,12 +15,55 @@ GoalSender::GoalSender(ros::NodeHandle &handle, int argc, char **argv) {
                                                                                this));
 
     ROS_INFO("goal_sender_callback  is set");
+
     rosLinkClientPtr->listen();
 }
 
+template<typename T>
+std::vector<T> splitByDelimiter(const std::string &s, char delim);
+
+template<>
+std::vector<double> splitByDelimiter(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<double> result;
+    while (std::getline(ss, item, delim)) {
+        result.push_back(std::stod(item));
+    }
+    return result;
+}
+
+void GoalSender::parseTransforms(const std::map<std::string, std::string> &keyToValue) {
+
+    std::string goal1 = keyToValue.at(GOAL1);
+    std::string goal2 = keyToValue.at(GOAL2);
+    std::string goal3 = keyToValue.at(GOAL3);
+    std::string goal4 = keyToValue.at(GOAL4);
+
+    std::vector<double> goal = splitByDelimiter<double>(goal1, ';');
+    ROS_INFO("The 2d_goal1: x=%f, y=%f, theta=%f",goal[0], goal[1], goal[2]);
+    goal = splitByDelimiter<double>(goal2, ';');
+    ROS_INFO("The 2d_goal1: x=%f, y=%f, theta=%f",goal[0], goal[1], goal[2]);
+    goal = splitByDelimiter<double>(goal3, ';');
+    ROS_INFO("The 2d_goal1: x=%f, y=%f, theta=%f",goal[0], goal[1], goal[2]);
+    goal = splitByDelimiter<double>(goal4, ';');
+    ROS_INFO("The 2d_goal1: x=%f, y=%f, theta=%f",goal[0], goal[1], goal[2]);
+
+}
 
 void GoalSender::goalCallback(const interface::ProcessInterface::Parameters &params) {
     ROS_INFO("goal_sender_node is working");
+    auto data = params.key_value;
+    std::map<std::string, std::string> keyToValue;
+    for (const auto &a : data) {
+        keyToValue[a.key] = a.value;
+    }
+    //delta = std::stod(keyToValue[DELTA]);
+    num_goal = std::stod(keyToValue[NUM_GOAL]);
+    ROS_INFO("The number of goal = %d", num_goal);
+    parseTransforms(keyToValue);
+    //publishTrajectory();
+    //ROS_INFO("Got goal with delta = %f", delta);
     /*
     auto data = params.key_value;
     std::map<std::string, std::string> keyToValue;
